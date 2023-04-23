ecb0dc6b48177a03b65a02c09d371edad6c68d17
Patrice CAVEZZAN <pcavezzan@gmail.com>
2023-04-23 20:25:49 +0200

    Added test simple redux.

--- src/redux/pokemon/ApiMiddleware.test.ts
+++ src/redux/pokemon/ApiMiddleware.test.ts
@@ -1,16 +1,57 @@
-import { loadPokemon } from "./ApiMiddleware";
-import { MiddlewareAPI } from "redux";
-import { LOAD_POKEMON, LOAD_POKEMON_SUCCESS, LoadPokemonAction, LoadPokemonSuccessAction } from "./Actions";
+import {loadPokemon} from "./ApiMiddleware";
+import {
+    LOAD_POKEMON, LOAD_POKEMON_FAILURE,
+    LOAD_POKEMON_SUCCESS,
+    LoadPokemonAction, LoadPokemonFailureAction,
+    LoadPokemonSuccessAction
+} from "./Actions";
+import * as PokemonApi from '../../services/pokemon';
+import {Pokemon} from "../../types";
+import {MiddlewareAPI} from "redux";
 
 describe('pokemon/ApiMiddleware', () => {
-    test('should dispatch load pokemon success action on load pokemon action when api return pokemon successfully', () => {
-        const dispatchMock = jest.fn().mockReturnThis();
-        const middlewareApi: MiddlewareAPI = { dispatch: dispatchMock, getState: () => ({}) };
-        const loadPokemonMiddleWare = loadPokemon(middlewareApi);
-        const action: LoadPokemonAction = { type: LOAD_POKEMON, pokemonName: 'pokemon' };
+    test('should dispatch load pokemon success action on load pokemon action when api return pokemon successfully', async () => {
+        const pokemon: Pokemon = {
+            abilities: [{ability: {name: 'power', url: 'https://google.com'}}],
+            stats: [{stat: {name: 'fire'}, base_state: 1}],
+            sprites: {front_default: ''}
+        };
+        jest.spyOn(PokemonApi, 'getPokemonByName').mockResolvedValue(pokemon);
+        const { store, loadPokemon } = aLoadPokemonMiddleWare();
+        const action: LoadPokemonAction = {type: LOAD_POKEMON, pokemonName: 'pokemon'};
 
-        const nextAction = loadPokemonMiddleWare(dispatchMock)(action);
+        await loadPokemon(action);
 
-        expect(nextAction).toEqual({ type: LOAD_POKEMON_SUCCESS, pokemon: {} } as LoadPokemonSuccessAction)
+        expect(store.dispatch).toHaveBeenCalledWith({
+            type: LOAD_POKEMON_SUCCESS,
+            pokemon: {
+                sprites: { front_default: '' },
+                stats: [{ stat: { name: 'fire' } , base_state: 1}],
+                abilities: [{ name: 'power', url: 'https://google.com' }]
+            }
+        } as LoadPokemonSuccessAction);
     });
-})
+
+    test('should dispatch load pokemon failure action on load pokemon action when api call fails', async () => {
+        jest.spyOn(PokemonApi, 'getPokemonByName').mockRejectedValue('error');
+        const { store, loadPokemon } = aLoadPokemonMiddleWare();
+        const action: LoadPokemonAction = {type: LOAD_POKEMON, pokemonName: 'pokemon'};
+
+        await loadPokemon(action);
+
+        expect(store.dispatch).toHaveBeenCalledWith({type: LOAD_POKEMON_FAILURE} as LoadPokemonFailureAction);
+    });
+
+    const aLoadPokemonMiddleWare = () => {
+        const store: MiddlewareAPI = {
+            getState: jest.fn(),
+            dispatch: jest.fn()
+        }
+        const next = jest.fn()
+
+        const invoke = (action: any) => loadPokemon(store)(next)(action)
+
+        return { store, next, loadPokemon: invoke }
+    }
+
+});
